242ae8f6428a7388af8d22d70a39d1ae265760f8
India Thornton <rthornton1314@gmail.com>
2022-06-23 14:59:24 -0400

    arrays work 6/23

--- arrays.js
+++ arrays.js
@@ -12,3 +12,60 @@ let backpack = []
 backpack.push('sword', 'shield', 'food')
 
 console.log(backpack)
+
+//Realizing that his backpack is going to be too full to add other essentials, Jon decides to take his sword out and place it on his belt. Remove this sword from the backpack.
+
+let belt = backpack.splice(0,1)
+
+//console.log(belt)
+
+// Now Jon decides he wants to add his fur coat to his backpack. Create a variable called furCoat and give it a string value of "fur coat". Then add that variable to the backpack.
+
+let furCoat = 'fur coat';
+
+backpack.unshift(furCoat)
+
+console.log(backpack)
+
+//Realizing it is freezing outside, Jon decides to take the fur coat back out and put it on. Without using splice, remove the fur coat from the array.
+
+backpack.shift()
+
+
+
+//Let's check how much stuff we have in our backpack. Create a variable called itemCount and set it equal to the number of items in the backpack.
+
+let itemCount = backpack.length
+
+console.log(itemCount)  
+console.log(backpack[0])  //prints shield
+
+console.log(backpack[backpack.length -1])
+
+//There is currently no where near enough supplies to make the journey to Winterfell. Let's add some other essentials. Add the strings: flint, blanket, knife, and toothbrush.
+
+backpack.push('flint',"blanket", "knife", "toothbrush")
+console.log(backpack)
+
+// Realizing his bag is overflowing, Jon decides to split up his content into two bags. Create a backpack2 variable that will hold some items. Use the splice method to move the flint, blanket, and knife from backpack to backpack2
+
+let backpack2 = backpack.splice(backpack.length -4,4)
+
+console.log(backpack2)
+
+//Using bracket syntax, console.log the first item in backpack2
+
+console.log(backpack2[0])
+
+//LOOPS
+
+//Let's see the contents of Jon's backpacks. Use a for loop on each backpack to console.log each item
+
+for (let i= 0; i < backpack.length; i++){
+    console.log(backpack[1])
+}
+
+for(let p = 0 ; p < backpack2.length ; p++){
+    console.log(backpack2[p])
+}
+
