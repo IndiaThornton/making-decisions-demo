f9d0d9dfa01890fa75a4a722a888d2e45e843421
India Thornton <rthornton1314@gmail.com>
2022-06-23 12:43:49 -0400

    Array work with file from 6/22

--- arrays.js
+++ arrays.js
@@ -2,3 +2,13 @@
     In this file, we'll be continuing our story
     with Jon being allowed to flee his fight with Jamie.
 */
+
+//Jon, embarrassed, intends to head back to Winterfell asap. First, let's create a backpack for him so he can begin to pack his bags. Create a variable called backpack, that is equal to an empty array.
+
+let backpack = []
+
+//Now, let's add some of his stuff to that backpack. Add 3 strings to the array, "sword", "shield", and "food".
+
+backpack.push('sword', 'shield', 'food')
+
+console.log(backpack)
